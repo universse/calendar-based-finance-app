72b8965ce279c2510d259a32374172df5d1fcc6c
Phuoc <phuoc317049@gmail.com>
2017-01-31 01:37:46 +0800

    Fix express server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,14 @@
 var express = require('express')
+var expressStaticGzip = require('express-static-gzip')
 const paths = require('../config/paths')
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(express.static(paths.build))
+app.use('/', expressStaticGzip(paths.build))
 
 app.get('*', function (req, res) {
   res.sendFile(paths.builtHtml)
-})
-
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
-  next()
-})
-
-app.get('*.css', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
-  next()
 })
 
 app.listen(PORT, function () {
