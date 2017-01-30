5f2beee90c744519c053a3eadfec1851a4a7ca99
Phuoc <phuoc317049@gmail.com>
2017-01-31 01:27:11 +0800

    configure express to serve gzip

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ const paths = require('../config/paths')
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url)
-  } else {
-    next()
-  }
-})
-
 app.use(express.static(paths.build))
 
 app.get('*', function (req, res) {
   res.sendFile(paths.builtHtml)
+})
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
+})
+
+app.get('*.css', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
 })
 
 app.listen(PORT, function () {
